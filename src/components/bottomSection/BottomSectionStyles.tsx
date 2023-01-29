61e665a7272f2924056ba5aafa138e6a6b0a664b
Kweayon  Clark <Kweayon@gmail.com>
2023-01-28 23:38:16 -0600

    Bottom Section Styles

---
+++ src/components/bottomSection/BottomSectionStyles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { Row } from "../../styles/layout/Flex";
+
+const BottomSectionMainContainer = styled(Row)`
+
+    width: 100%;
+
+    height: 15%;
+
+    background: #a7b087;
+
+    align-items: flex-start;
+
+    justify-content: flex-start;
+
+    gap: 5%;
+
+`;
+
+export {
+      BottomSectionMainContainer
+    , 
+}
