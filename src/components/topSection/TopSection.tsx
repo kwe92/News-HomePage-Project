17bea4b13604f2927155fc5e71c4ab6194e165a4
Kweayon  Clark <Kweayon@gmail.com>
2023-01-28 23:38:34 -0600

    Top Section Update

--- src/components/topSection/TopSection.tsx
+++ src/components/topSection/TopSection.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { ContentContainer1, ContentContainer2, TopSectionMainContainer } from "./topSectionStyles";
+import { images } from "../../constants/images";
+import { ContentContainer1, ContentContainer2, TopSectionMainContainer, TopImageContainer, TopContainerImage, TopContainerBody, ContentTitle } from "./topSectionStyles";
 
 const TopSection = (props:{}) => {
     return(
         <TopSectionMainContainer>
 
             <ContentContainer1>
-                ContentContainer1
+                <TopImageContainer />
+                <TopContainerBody>
+                    <ContentTitle>
+                    The Bright Future of Web3.0?
+                    </ContentTitle>
+                </TopContainerBody>              
             </ContentContainer1>
 
             <ContentContainer2>
