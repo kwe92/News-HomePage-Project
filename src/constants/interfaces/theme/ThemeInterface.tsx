62c6f639f051c82c4c42b6f5fe9c2d5ee6a00d26
Kweayon  Clark <Kweayon@gmail.com>
2023-01-28 22:27:01 -0600

    Update

---
+++ src/constants/interfaces/theme/ThemeInterface.tsx
@@ -0,0 +1,12 @@
+export default interface ThemeInterface {
+    softOrangeColor: string,
+    softRedColor: string,
+    offWhiteColor: string,
+    greyishBlueColor: string,
+    darkGreyishBlue: string,
+    veryDarkBlueColor: string,
+    bodyFontSize: string,
+    FontWeightlight: number,
+    FontWeightMedium: number,
+    FontWeightHeavy: number
+};
