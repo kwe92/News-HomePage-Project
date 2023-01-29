3dae99838f45024b57c4ccf1f927b0691df78e30
Kweayon  Clark <Kweayon@gmail.com>
2023-01-28 23:37:57 -0600

    Bottom Section Component

---
+++ src/components/bottomSection/BottomSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BottomSectionMainContainer } from "./BottomSectionStyles";
+
+const BottomSection = (props:{}) => {
+    return (
+        <BottomSectionMainContainer>
+            Bottom Section
+        </BottomSectionMainContainer>
+    );
+};
+
+export default BottomSection;
