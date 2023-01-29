39d98ab2848314f363abae176ed174f7db537551
Kweayon  Clark <Kweayon@gmail.com>
2023-01-28 22:26:30 -0600

    Image Default Export

---
+++ src/constants/images/Images.tsx
@@ -0,0 +1,21 @@
+import favicon from "../../assets/images/favicon-32x32.png";
+import iconMenuClose from "../../assets/images/icon-menu-close.svg";
+import iconMenu from "../../assets/images/icon-menu.svg";
+import logo from "../../assets/images/logo.svg";
+import imageGamingGrowth from "../../assets/images/image-gaming-growth.jpg";
+import imageRetroPcs from "../../assets/images/image-retro-pcs.jpg";
+import imageTopLaptops from "../../assets/images/image-top-laptops.jpg";
+import imageWeb3Desktop from "../../assets/images/image-web-3-desktop.jpg";
+import imageWeb3Mobiile from "../../assets/images/image-web-3-mobile.jpg";
+
+export default {
+      favicon
+    , iconMenuClose
+    , iconMenu
+    , logo
+    , imageGamingGrowth
+    , imageRetroPcs
+    , imageTopLaptops
+    , imageWeb3Desktop
+    , imageWeb3Mobiile
+}
