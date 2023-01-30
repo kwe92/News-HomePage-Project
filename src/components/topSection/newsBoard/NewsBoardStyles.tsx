140ddde4cacdbbc44b96be441fc749601c0328f6
Kweayon  Clark <Kweayon@gmail.com>
2023-01-30 07:39:04 -0600

    Added More Styles
    Added ListTiles

--- src/components/topSection/newsBoard/NewsBoardStyles.tsx
+++ src/components/topSection/newsBoard/NewsBoardStyles.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import ThemeInterface from "../../../constants/interfaces/theme/ThemeInterface";
 import { Column } from "../../../styles/layout/Flex";
+
+// TODO: Finish adding color
 
 const NewsBoardContainer = styled(Column)`
 
@@ -15,17 +18,59 @@ const NewsBoardContainer = styled(Column)`
 
 `;
 
+const NewsBoardInnerContainer = styled(Column)`
+    align-items: flex-start;
+    justify-content: space-between;
+
+    height: 100%;
+    width: 100%;
+    padding: 2rem;
+
+`;
+
 const NewsBoardTitle = styled.h1`
 
 `;
 
-const NewsBoardListTile = styled(Column)``;
+const NewsBoardListTile = styled(Column)`
 
-const HorizontalLine = styled.div``;
+    width: 100%;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 0.75rem;
+
+`;
+
+const ListTileTop = styled.h3`
+
+`;
+
+const ListTileBottom = styled.p`
+
+    font-size: ${({ theme }: { theme: ThemeInterface }) => (theme.bodyFontSize)};
+
+    color: ${({ theme }: { theme: ThemeInterface }) => (theme.greyishBlueColor)};
+
+
+`;
+
+
+
+
+const HorizontalLine = styled.div`
+
+    background: ${({ theme }:{ theme:ThemeInterface }) => (theme.greyishBlueColor)};
+    width: 100%;
+    height: 0.0625rem;
+
+`;
 
 export {
       NewsBoardContainer
     , NewsBoardTitle
     , NewsBoardListTile
     , HorizontalLine
+    , NewsBoardInnerContainer
+    , ListTileTop
+    , ListTileBottom
 };
