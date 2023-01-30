524ec4f289150c7f2e0e23757021102e70a09659
Kweayon  Clark <Kweayon@gmail.com>
2023-01-30 07:38:25 -0600

    Added List Tiles

--- src/components/topSection/newsBoard/NewsBoard.tsx
+++ src/components/topSection/newsBoard/NewsBoard.tsx
@@ -1,29 +1,56 @@
 import React from "react";
-import { NewsBoardContainer, NewsBoardTitle, NewsBoardListTile, HorizontalLine
- } from "./NewsBoardStyles";
+import { NewsBoardContainer, NewsBoardTitle, NewsBoardListTile, HorizontalLine, NewsBoardInnerContainer, ListTileTop, ListTileBottom} from "./NewsBoardStyles";
 
 const NewsBoard = (props:{}) => {
     return (
         <NewsBoardContainer>
+
+            <NewsBoardInnerContainer>
+
             <NewsBoardTitle>
             New
             </NewsBoardTitle>
 
             <NewsBoardListTile>
-                ListTile1
+
+                <ListTileTop>
+                Hydrogen Vs Electric Cars
+                </ListTileTop>
+
+                <ListTileBottom>
+                    Will hydrogen fueled cars even catch up to EVs?
+                </ListTileBottom>
+
             </NewsBoardListTile>
 
             <HorizontalLine />
 
             <NewsBoardListTile>
-                ListTile2
+                <ListTileTop>
+                    The Downsides of AI Artistry
+                </ListTileTop>
+
+                <ListTileBottom>
+                    What are the possible adverse effects of on-demand AI image generation?
+                </ListTileBottom>
             </NewsBoardListTile>
 
             <HorizontalLine />
 
             <NewsBoardListTile>
-                ListTile3
+
+                <ListTileTop>
+                    Is VC Funding Drying Up?
+                </ListTileTop>
+
+                <ListTileBottom>
+                    Private funding  by VC firms is down 50% YOY. We take a look at what that means.
+                </ListTileBottom>
+
             </NewsBoardListTile>
+
+            </NewsBoardInnerContainer>
+           
 
         </NewsBoardContainer>
     );
