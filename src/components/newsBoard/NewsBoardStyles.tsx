76b183b2c02b56df0a2d1e94c9847adc831124a6
Kweayon  Clark <Kweayon@gmail.com>
2023-01-29 23:49:31 -0600

    News Board Styles

---
+++ src/components/newsBoard/NewsBoardStyles.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Column } from "../../styles/layout/Flex";
+
+const NewsBoardContainer = styled(Column)`
+
+    align-items: flex-start;
+
+    justify-content: flex-start;
+
+    height: 100%;
+
+    width: 34.5%;
+
+    background: #cd8b62;
+
+`;
+
+const NewsBoardTitle = styled.h1`
+
+`;
+
+const NewsBoardListTile = styled(Column)``;
+
+const HorizontalLine = styled.div``;
+
+export {
+      NewsBoardContainer
+    , NewsBoardTitle
+    , NewsBoardListTile
+    , HorizontalLine
+};
