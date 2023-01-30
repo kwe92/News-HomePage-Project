f12fc8ae3cef694f3fbbc63d2c39f58fa2ce69bf
Kweayon  Clark <Kweayon@gmail.com>
2023-01-29 23:49:44 -0600

    Read More Button

---
+++ src/components/topSection/readMoreButton/ReadMoreButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyledButton, ButtonBody } from "./ReadMoreButtonStyles";
+
+const ReadMoreButton = (props:{}) => {
+    return (
+        <StyledButton>
+            <ButtonBody>
+            read more
+            </ButtonBody>
+        </StyledButton>
+    );
+};
+
+export default ReadMoreButton;
