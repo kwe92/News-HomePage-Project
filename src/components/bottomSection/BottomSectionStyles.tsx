1adf0d95b83a4e845d81e90c47ff4e2d070c4720
Kweayon  Clark <Kweayon@gmail.com>
2023-01-30 21:56:25 -0600

    Updated Styles

--- src/components/bottomSection/BottomSectionStyles.tsx
+++ src/components/bottomSection/BottomSectionStyles.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import { Row } from "../../styles/layout/Flex";
+import { Row, Column } from "../../styles/layout/Flex";
+import { images } from "../../constants/images";
+import ThemeInterface from "../../constants/interfaces/theme/ThemeInterface";
 
 const BottomSectionMainContainer = styled(Row)`
 
@@ -7,17 +9,73 @@ const BottomSectionMainContainer = styled(Row)`
 
     height: 15%;
 
-    background: #a7b087;
+    margin-top: 1rem;
 
     align-items: flex-start;
 
     justify-content: flex-start;
 
+    gap: 2%;
+
+    // background: #a7b087;
+
+
+`;
+
+const ArticleListTile = styled(Row)`
+
+    height: 100%;
+    width: 33.33%;
+    align-items: flex-start;
+    justify-content: flex-start;
     gap: 5%;
+    // background: lightblue;
+
+`;
+
+const ListTileImage = styled.img`
+
+    height: 100%;
+    width: 30%;
+
+`;
+
+const ListTileTop = styled.h1`
+ color: ${({ theme }:{ theme:ThemeInterface }) => (theme.greyishBlueColor)};
+`;
+
+const ListTileMiddle = styled.h4`
+
+color: ${({ theme }:{ theme:ThemeInterface }) => (theme.veryDarkBlueColor)};
+font-weight: ${({ theme }:{ theme:ThemeInterface }) => (theme.FontWeightHeavy)};
+`;
+
+const ListTileBottom = styled.p`
+
+color: ${({ theme }:{ theme:ThemeInterface }) => (theme.greyishBlueColor)};
+font-size: ${({ theme }:{ theme:ThemeInterface }) => (theme.bodyFontSize)};
+// font-weight: ${({ theme }:{ theme:ThemeInterface }) => (theme.FontWeightHeavy)};
+
+`;
+
+const ListTileBodyContainer = styled(Column)`
+
+align-items: flex-start;
+
+justify-content: space-between;
+
+height: 100%;
+
+width: 70%;
 
 `;
 
 export {
       BottomSectionMainContainer
-    , 
+    , ArticleListTile
+    , ListTileImage
+    , ListTileTop
+    , ListTileMiddle
+    , ListTileBottom
+    , ListTileBodyContainer
 }
