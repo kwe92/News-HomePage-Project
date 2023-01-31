99c4110d8f8802bd5c7f135b3f8e5a520c77f459
Kweayon  Clark <Kweayon@gmail.com>
2023-01-30 21:56:06 -0600

    Added ListTiles and Styling

--- src/components/bottomSection/BottomSection.tsx
+++ src/components/bottomSection/BottomSection.tsx
@@ -1,10 +1,62 @@
 import React from "react";
-import { BottomSectionMainContainer } from "./BottomSectionStyles";
+import { BottomSectionMainContainer ,ArticleListTile ,ListTileImage , ListTileTop, ListTileMiddle, ListTileBottom, ListTileBodyContainer } from "./BottomSectionStyles";
+import { images } from "../../constants/images";
 
 const BottomSection = (props:{}) => {
     return (
         <BottomSectionMainContainer>
-            Bottom Section
+
+            <ArticleListTile>
+
+                <ListTileImage src={images.imageRetroPcs} alt="imageRetroPcs"/>
+                <ListTileBodyContainer>
+                    <ListTileTop>
+                        01
+                    </ListTileTop>
+                    <ListTileMiddle>
+                    Reviving Retro Pcs
+                    </ListTileMiddle>
+
+                    <ListTileBottom>
+                        What happens when old PCs are given modern upgrades?
+                    </ListTileBottom>
+                </ListTileBodyContainer>
+              
+            </ArticleListTile>
+
+            <ArticleListTile>
+            <ListTileImage src={images.imageTopLaptops} alt="imageTopLaptops"/>
+                <ListTileBodyContainer>
+                    <ListTileTop>
+                        02
+                    </ListTileTop>
+                    <ListTileMiddle>
+                        Top 10 Laptops of 2022
+                    </ListTileMiddle>
+
+                    <ListTileBottom>
+                        Our best picks for various needs and budgets.
+                    </ListTileBottom>
+                </ListTileBodyContainer>
+            </ArticleListTile>
+
+            <ArticleListTile>
+                <ListTileImage src={images.imageGamingGrowth} alt="imageGamingGrowth"/>
+
+                <ListTileBodyContainer>
+                    <ListTileTop>
+                        03
+                    </ListTileTop>
+                    <ListTileMiddle>
+                        The Growth of Gaming
+                    </ListTileMiddle>
+
+                    <ListTileBottom>
+                        How the pandemic sparked fresh opportunities.
+                    </ListTileBottom>
+                </ListTileBodyContainer>
+            </ArticleListTile>
+
         </BottomSectionMainContainer>
     );
 };
