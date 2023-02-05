2a3dfb57dd1ba4fa317dec652fefac193d392826
Kweayon  Clark <Kweayon@gmail.com>
2023-02-04 23:54:45 -0600

    Added a todo

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,16 @@ import Navbar from "./components/navbar/Navbar";
 import { AppContainer } from "./AppStyles";
 import TopSection from "./components/topSection/TopSection";
 import BottomSection from "./components/bottomSection/BottomSection";
-
-const App = (props:{}) => {
-    return(
-        <AppContainer>
-            <AppGlobalTheme />
-            <Navbar />
-            <TopSection />
-            <BottomSection />
-        </AppContainer>
-       
-    );
+// TODO: Add created by at the bottom of the page
+const App = (props: {}) => {
+  return (
+    <AppContainer>
+      <AppGlobalTheme />
+      <Navbar />
+      <TopSection />
+      <BottomSection />
+    </AppContainer>
+  );
 };
 
 export default App;
