23c0ab976c7fbe7f3a1d5a4bb917a75cd8bae20e
Kweayon  Clark <Kweayon@gmail.com>
2023-02-04 23:42:08 -0600

    Added Hover Effects

--- src/components/topSection/readMoreButton/ReadMoreButtonStyles.tsx
+++ src/components/topSection/readMoreButton/ReadMoreButtonStyles.tsx
@@ -2,28 +2,27 @@ import styled from "styled-components";
 import ThemeInterface from "../../../constants/interfaces/theme/ThemeInterface";
 
 const StyledButton = styled.button`
+  height: 2.5625rem;
 
-    height: 2.5625rem;
+  width: 47.5%;
 
-    width: 47.5%;
+  color: ${({ theme }: { theme: ThemeInterface }) => theme.offWhiteColor};
 
-    color: ${({ theme }:{ theme:ThemeInterface }) => (theme.offWhiteColor)};
+  background: ${({ theme }: { theme: ThemeInterface }) => theme.softRedColor};
 
-    background: ${({ theme }: {theme:ThemeInterface}) => (theme.softRedColor)};
+  text-transform: uppercase;
 
-    text-transform: uppercase;
+  letter-spacing: 0.1875rem;
 
-    letter-spacing: 0.1875rem;
+  border: none;
 
-    border: none;
+  &: hover {
+    transition: all 0.3125s ease-in-out;
+    background: ${({ theme }: { theme: ThemeInterface }) =>
+      theme.darkGreyishBlue};
+  }
 `;
 
-const ButtonBody = styled.p`
+const ButtonBody = styled.p``;
 
-
-`;
-
-export {
-      StyledButton
-    , ButtonBody
-};
+export { StyledButton, ButtonBody };
